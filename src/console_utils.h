bbf007825c33d8435766c4d47fce02dfee1844e0
michal <michal@lonski.pl>
2018-03-11 14:19:37 -0400

    reading user input

---
+++ src/console_utils.h
@@ -0,0 +1,43 @@
+#ifndef INPUTUTILS_H
+#define INPUTUTILS_H
+
+void clear_cin() {
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template <typename T>
+T getNumber(const std::string& msg) {
+  std::cout << msg << "\n>";
+  T val;
+  while (!(std::cin >> val)) {
+    clear_cin();
+    std::cout << "Invalid input. Try again.\n>";
+  }
+  return val;
+}
+
+template <typename T>
+std::vector<T> getVector(const std::string& msg) {
+  std::string line;
+
+  std::cout << msg << " (podaj liczby oddzielone spacjami)\n>";
+  std::getline(std::cin, line);
+
+  T number;
+  std::vector<T> numbers;
+  std::istringstream stream(line);
+  while (stream >> number)
+    numbers.push_back(number);
+
+  return numbers;
+}
+
+bool getBool(const std::string& msg) {
+  std::cout << msg << " [t/n]\n>";
+  std::string s;
+  std::cin >> s;
+  return s == "t";
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
+#include <limits>
+#include <sstream>
 #include "activation_functions.h"
 #include "neural_net.h"
+#include "console_utils.h"
+
+using namespace std;
 
 int main() {
+  bool areParametersAccepted = false;
   nn::NeuralNetwork::Parameters params;
-  params.input = {0.05, 0.1};
-  params.expectedOutput = {0.01, 0.99};
-  params.hiddenLayerCount = 2;
-  params.hiddenLayerNeuronsCount = 3;
-  params.toleratedError = 0.01;
-  params.activationFn = nn::ActivationFunctions::LOGISTIC;
-
+  std::cout << " :: Neuronomator 6000 ::" << std::endl;
+  
+  while(!areParametersAccepted) {
+    std::cout << "--------------------------------------------------" << std::endl;
+    params.input = getVector<double>("Podaj wartości neuronów w warstwie wejściowej");
+    params.expectedOutput = getVector<double>("Podaj spodziewane wartości neuronów w warstwie wyjściowej");
+    params.hiddenLayerCount = getNumber<int>("Podaj liczbe warstw ukrytych");
+    params.hiddenLayerNeuronsCount = getNumber<int>("Podaj liczbe neuronow w warstwach ukrytych");
+    params.toleratedError = getNumber<double>("Podaj wartosc bledu granicznego");
+    params.activationFn = nn::ActivationFunctions::LOGISTIC;
+        
+    //print parameters
+    //  -> możnaby narysowac siec w ascii :-D
+    //     IN  | H1 | H2 | OUT
+    //   ---------------------
+    //          |x|  |x|
+    //   |0.1|  |x|  |x|  |0.05|
+    //   |0.3|  |x|  |x|  |0.99|
+    //          |x|  |x|
+    
+    areParametersAccepted = getBool("Ok?");
+    clear_cin();
+  }
+  
+  int e = getNumber<int>("Okresl maksymalna liczbe epok");
   nn::NeuralNetwork* nn = new nn::NeuralNetwork(params);
-  nn->learn(1000);
+  nn->learn(e);
 }
+
+// moźnaby jeszcze dodać printowanie tych parametrów i zapytać usera czy 'ok', a jak nie to powtórzyć cały proces wczytywania
